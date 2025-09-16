4a37d5bd110ad03c466b86935a2bc136ae1f6c50
Egor Novikov <egoriyNovikov@gmail.com>
2025-09-16 20:36:09 +0300

    final

--- cmd/main.go
+++ cmd/main.go
@@ -4,26 +4,25 @@ import (
 	"egoriynovikov/todo_api/internal/config"
 	"egoriynovikov/todo_api/internal/db"
 	"egoriynovikov/todo_api/internal/router"
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	cfgd := config.GetConfigDB()
-	fmt.Println(cfgd)
 	cfgs := config.GetConfigServer()
 	port := cfgs.Port
 	db, err := db.Connect(cfgd)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer db.Close()
 	if port == "" {
 		port = "8080"
 	}
 
-	router.NewRouter(port)
+	router.NewRouter(port, db)
 
 	log.Printf("Сервер запущен на порту %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,11 +1,15 @@
 package router
 
 import (
+	"database/sql"
+	"egoriynovikov/todo_api/internal/handlers"
 	"fmt"
 	"net/http"
 )
 
-func NewRouter(port string) {
+func NewRouter(port string, db *sql.DB) {
+	taskController := handlers.NewTaskController(db)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Todo API работает на порту %s", port)
 	})
@@ -13,5 +17,28 @@ func NewRouter(port string) {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "OK")
+	})
+
+	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			taskController.GetAllTasks(w, r)
+		case "POST":
+			taskController.CreateTask(w, r)
+		default:
+			http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.HandleFunc("/task", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			taskController.GetTask(w, r)
+		} else if r.Method == "PUT" {
+			taskController.UpdateTask(w, r)
+		} else if r.Method == "DELETE" {
+			taskController.DeleteTask(w, r)
+		} else {
+			http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+		}
 	})
 }
